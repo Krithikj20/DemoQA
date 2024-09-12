08e11fc76ffcc8d0cef6295406157202a770848f
krithikj20 <krithik@geekyants.com>
2024-09-12 13:47:37 +0530

    Automated slider in widgets

--- src/main/java/org/example/WidgetsPageObjects.java
+++ src/main/java/org/example/WidgetsPageObjects.java
@@ -3,8 +3,13 @@ package org.example;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class WidgetsPageObjects {
     public WebDriver driver;
@@ -13,18 +18,30 @@ public class WidgetsPageObjects {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-@FindBy(xpath = "//div[contains(text(),'Widgets')]")
+    @FindBy(xpath="(//div[@class='card mt-4 top-card'])[4]")
+    private WebElement WidgetsCard;
+@FindBy(xpath = "(//div[@class='header-wrapper'])[4]")
     private WebElement widgets;
-@FindBy(xpath="(//div[@class='card mt-4 top-card'])[4]")
-private WebElement WidgetsCard;
+
 @FindBy(xpath="//span[contains(text(),'Slider')]")
-private WebElement Slider;
+private WebElement SliderTab;
+@FindBy(xpath="//input[@class='range-slider range-slider--primary']")
+private WebElement SliderTooltip;
 
     public void slider() {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", WidgetsCard);
         WidgetsCard.click();
-        widgets.click();
-        Slider.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(widgets));
+//    widgets.click();
+//        widgets.click();
+//        wait.until(ExpectedConditions.visibilityOf(SliderTab));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", SliderTab);
+
+     SliderTab.click();
+        Actions moveSlider = new Actions(driver);
+  moveSlider.clickAndHold(SliderTooltip).moveByOffset(0, 40).release().perform(); // Adjust the x-offset value as needed
+
 
     }
 
