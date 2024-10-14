d436a7b9a7e273779727adcf0a2db097aa6b2150
krithikj20 <krithik@geekyants.com>
2024-10-14 18:28:06 +0530

    Automated Four Alert Flows

--- src/main/java/org/example/AlertsPageObjects.java
+++ src/main/java/org/example/AlertsPageObjects.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+
 
 public class AlertsPageObjects {
     public WebDriver driver;
@@ -22,30 +24,59 @@ public class AlertsPageObjects {
 
     @FindBy(xpath = "(//div[@class='card mt-4 top-card'])[3]")
     public WebElement Card;
-@FindBy(xpath = "(//*[contains(text(),'Alerts')])[2]")
+    @FindBy(xpath = "(//*[contains(text(),'Alerts')])[2]")
     public WebElement AlertsTab;
 
-@FindBy(xpath = "//button[@id='alertButton']")
-public WebElement AlertButton;
+    @FindBy(xpath = "//button[@id='alertButton']")
+    public WebElement AlertButton;
 
-@FindBy(xpath = "//button[@id='timerAlertButton']")
-public WebElement Alert5SecondsButton;
+    @FindBy(xpath = "//button[@id='timerAlertButton']")
+    public WebElement Alert5SecondsButton;
+    @FindBy(xpath = "//button[@id='confirmButton']")
+    public WebElement Alert3rdButton;
+    @FindBy(xpath = "//span[@id='confirmResult']")
+    public WebElement greenText;
 
-public void alertsFlow() {
-    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Card);
-    Card.click();
-    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", AlertsTab);
-    AlertsTab.click();
-    AlertButton.click();
-    driver.switchTo().alert().accept();
-}
-public void Alert5Seconds()
-{
-    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Alert5SecondsButton);
-    Alert5SecondsButton.click();
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    wait.until(ExpectedConditions.alertIsPresent());
-    driver.switchTo().alert().accept();
 
-}
+    @FindBy(xpath = "//button[@id='promtButton']")
+    public WebElement Alert4Button;
+    @FindBy(xpath = "//span[@id='promptResult']")
+    public WebElement Text4;
+
+    public void alertsFlow() {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Card);
+        Card.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", AlertsTab);
+        AlertsTab.click();
+        AlertButton.click();
+        driver.switchTo().alert().accept();
+    }
+
+    public void Alert5Seconds() {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Alert5SecondsButton);
+        Alert5SecondsButton.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().accept();
+
+    }
+
+    public String Alert3Buttonflow() {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Alert3rdButton);
+        Alert3rdButton.click();
+        driver.switchTo().alert().dismiss();
+        String text3 = greenText.getText();
+
+        return text3;
+    }
+
+    public String Alert4ButtonFlow() {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Alert4Button);
+        Alert4Button.click();
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys("krithi");
+        driver.switchTo().alert().accept();
+        String text4 = Text4.getText();
+        return text4;
+    }
 }
--- src/test/java/Alerts.java
+++ src/test/java/Alerts.java
@@ -1,6 +1,9 @@
 import org.example.AlertsPageObjects;
 import org.example.ElementsPageObjects;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class Alerts extends BaseTest{
 
@@ -9,6 +12,12 @@ public class Alerts extends BaseTest{
             AlertsPageObjects alert=new AlertsPageObjects(driver);
     alert.alertsFlow();
     alert.Alert5Seconds();
+
+    String CancelText= alert.Alert3Buttonflow();
+    Assert.assertEquals(CancelText, "You selected Cancel");
+
+    String PromptboxText=alert.Alert4ButtonFlow();
+    Assert.assertEquals(PromptboxText, "You entered krithi");
     }
 
 }
